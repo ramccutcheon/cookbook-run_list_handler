da9177cf12c40b7eb27ed6cf6bad26e523c790c7
torandu <sean@torandu.org>
2014-03-28 16:33:11 -0700

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jhtran@att.com'
 license          'All rights reserved'
 description      'Installs/Configures run_list_handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 depends 'chef_handler'
